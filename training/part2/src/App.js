6782f3f1bdd0667a3798435c306c2cceb4de88c5
Laurin Novak <laurin@pellervoinen.com>
2022-09-13 18:29:48 +0300

    do part 2c training

--- training/part2/src/App.js
+++ training/part2/src/App.js
@@ -1,10 +1,24 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import Note from './components/Note'
+import axios from 'axios'
 
-const App = (props) => {
-    const [notes, setNotes] = useState(props.notes)
+const App = () => {
+    const [notes, setNotes] = useState([])
     const [newNote, setNewNote] = useState('a new note...')
     const [showAll, setShowAll] = useState(true)
+
+    const hook = () => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/notes')
+            .then(response => {
+                console.log('promise fulfilled')
+                setNotes(response.data)
+            })
+    }
+
+    useEffect(hook, [])
+    console.log('render', notes.length, 'notes')
 
     const addNote = (event) => {
         event.preventDefault()
