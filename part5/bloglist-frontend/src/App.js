d4278b19a990bfe4fc533f567160bfddae58f93e
Laurin Novak <laurin@pellervoinen.com>
2022-09-28 15:37:24 +0300

    implement exercises 5.3-5.4

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -2,13 +2,21 @@ import {useEffect, useState} from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import Notification from './components/Notification'
+import Error from './components/Error'
 
 const App = () => {
     const [blogs, setBlogs] = useState([])
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [user, setUser] = useState(null)
+
+    const [notificationMessage, setNotificationMessage] = useState(null)
     const [errorMessage, setErrorMessage] = useState(null)
+
+    const [title, setTitle] = useState('')
+    const [author, setAuthor] = useState('')
+    const [url, setUrl] = useState('')
 
     useEffect(() => {
         blogService.getAll().then(blogs =>
@@ -21,7 +29,7 @@ const App = () => {
         if (loggedUserJson) {
             const user = JSON.parse(loggedUserJson)
             setUser(user)
-            // TODO setToken in blogService
+            blogService.setToken(user.token)
         }
     }, [])
 
@@ -51,6 +59,36 @@ const App = () => {
         setUser(null)
     }
 
+    const handleBlogCreation = async (event) => {
+        event.preventDefault()
+
+        try {
+            const blogReq = {
+                title,
+                author,
+                url
+            }
+
+            const blogRes = await blogService.create(blogReq)
+            setBlogs(prevState => [...prevState, blogRes])
+
+            setTitle('')
+            setAuthor('')
+            setUrl('')
+
+            setNotificationMessage('Created new blog')
+            setTimeout(() => {
+                setNotificationMessage(null)
+            }, 5000)
+
+        } catch (e) {
+            setErrorMessage('Creation failed')
+            setTimeout(() => {
+                setErrorMessage(null)
+            }, 5000)
+        }
+    }
+
     const loginForm = () => {
         return (
             <form onSubmit={handleLogin}>
@@ -77,10 +115,47 @@ const App = () => {
         )
     }
 
+    const blogForm = () => {
+        return (
+            <form onSubmit={handleBlogCreation}>
+                <div>
+                    title:
+                    <input
+                        type="text"
+                        value={title}
+                        name="Title"
+                        onChange={({target}) => setTitle(target.value)}
+                    />
+                </div>
+                <div>
+                    author:
+                    <input
+                        type="text"
+                        value={author}
+                        name="Author"
+                        onChange={({target}) => setAuthor(target.value)}
+                    />
+                </div>
+                <div>
+                    url:
+                    <input
+                        type="text"
+                        value={url}
+                        name="Url"
+                        onChange={({target}) => setUrl(target.value)}
+                    />
+                </div>
+                <button type="submit">create</button>
+            </form>
+        )
+    }
+
     if (user === null) {
         return (
             <div>
                 <h2>Log in to application</h2>
+                <Notification message={notificationMessage}/>
+                <Error message={errorMessage}/>
                 {loginForm()}
             </div>
         )
@@ -89,7 +164,15 @@ const App = () => {
     return (
         <div>
             <h2>blogs</h2>
+            <Notification message={notificationMessage}/>
+            <Error message={errorMessage}/>
+
             <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
+
+            <h2>create new</h2>
+
+            {blogForm()}
+
             {blogs.map(blog =>
                 <Blog key={blog.id} blog={blog}/>
             )}
