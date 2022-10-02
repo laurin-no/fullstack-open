1f66457e2e2e07beb3495ffa5aee6b6bebba3e1b
Laurin Novak <laurin@pellervoinen.com>
2022-10-02 18:01:30 +0300

    implement exercises 5.9

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -147,7 +147,7 @@ const App = () => {
 
             {blogForm()}
 
-            {blogs.map(blog =>
+            {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
                 <Blog key={blog.id} blog={blog} updateBlog={updateBlog}/>
             )}
         </div>
