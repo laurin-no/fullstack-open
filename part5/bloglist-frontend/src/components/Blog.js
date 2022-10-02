9aa006f5149d5138ee7b99bf83dd83dc00aabeda
Laurin Novak <laurin@pellervoinen.com>
2022-10-02 13:32:37 +0300

    implement exercises 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,34 @@
-const Blog = ({blog}) => (
-    <div>
-        {blog.title} {blog.author}
-    </div>
-)
+import {useState} from 'react'
+
+const Blog = ({blog}) => {
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+
+    const [visible, setVisible] = useState(false)
+    const showWhenVisible = {display: visible ? '' : 'none'}
+
+    const toggleVisibility = () => {
+        setVisible(!visible)
+    }
+
+    return (
+        <div style={blogStyle}>
+            {blog.title} {blog.author}
+            <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button>
+            <div style={showWhenVisible}>
+                {blog.url}<br/>
+                likes {blog.likes}
+                <button>like</button>
+                <br/>
+                {blog.user.name}<br/>
+            </div>
+        </div>
+    )
+}
 
 export default Blog
