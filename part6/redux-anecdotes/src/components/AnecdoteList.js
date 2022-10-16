421f711d20adef3daf2ad98639d431103ddc120c
Laurin Novak <laurin@pellervoinen.com>
2022-10-16 14:28:11 +0300

    implement exercises 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -17,7 +17,7 @@ const Anecdote = ({ anecdote, handleClick }) => {
 
 const AnecdoteList = () => {
     const dispatch = useDispatch()
-    const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes))
+    const anecdotes = useSelector(state => state.anecdotes.sort((a, b) => b.votes - a.votes))
 
     return (
         <ul>
