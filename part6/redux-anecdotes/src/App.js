bbfd14f080688bacae4ed0430180f84763c5c724
Laurin Novak <laurin@pellervoinen.com>
2022-10-17 15:45:32 +0300

    implement exercises 6.10

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,11 +1,13 @@
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
 
 const App = () => {
 
     return (
         <div>
             <h2>Anecdotes</h2>
+            <Notification/>
             <AnecdoteForm/>
             <AnecdoteList/>
         </div>
