d9e42d677121c336e6d74ba4502779e723ef18fa
Laurin Novak <laurin@pellervoinen.com>
2022-10-18 14:24:10 +0300

    implement exercises 6.15

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -4,13 +4,13 @@ import Notification from './components/Notification'
 import Filter from './components/Filter'
 import { useDispatch } from 'react-redux'
 import { useEffect } from 'react'
-import anecdoteService from './services/anecdotes'
-import { setAnecdotes } from './reducers/anecdoteReducer'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
     const dispatch = useDispatch()
+
     useEffect(() => {
-        anecdoteService.getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+        dispatch(initializeAnecdotes())
     }, [dispatch])
 
 
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 const anecdoteSlice = createSlice({
     name: 'anecdotes',
@@ -22,4 +23,12 @@ const anecdoteSlice = createSlice({
 })
 
 export const { vote, createAnecdote, setAnecdotes } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+    return async dispatch => {
+        const anecdotes = await anecdoteService.getAll()
+        dispatch(setAnecdotes(anecdotes))
+    }
+}
+
 export default anecdoteSlice.reducer
