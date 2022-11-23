d80083d31e68712e244f070eabe3f2762bbf3f36
Laurin Novak <laurin@pellervoinen.com>
2022-11-23 18:57:55 +0200

    implement exercises 8.7

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -114,6 +114,7 @@ const typeDefs = gql`
             author: String!
             genres: [String!]!
         ): Book
+        editAuthor(name: String!, setBornTo: Int!): Author
     }
 `
 
@@ -140,6 +141,18 @@ const resolvers = {
             }
 
             return book
+        },
+        editAuthor: (root, args) => {
+            const author = authors.find((a) => a.name === args.name)
+            if (!author) {
+                return null
+            }
+
+            const updatedAuthor = { ...author, born: args.setBornTo }
+            authors = authors.map((a) =>
+                a.name === args.name ? updatedAuthor : a
+            )
+            return updatedAuthor
         },
     },
 }
