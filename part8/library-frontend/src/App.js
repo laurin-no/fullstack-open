6f6784d37957b43799232953ecb6c6837c1b55e7
Laurin Novak <laurin@pellervoinen.com>
2022-11-24 16:59:24 +0200

    implement exercises 8.9

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -3,12 +3,12 @@ import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import { useQuery } from '@apollo/client'
-import { ALL_AUTHORS } from './queries'
+import { ALL_AUTHORS_AND_BOOKS } from './queries'
 
 const App = () => {
     const [page, setPage] = useState('authors')
 
-    const result = useQuery(ALL_AUTHORS)
+    const result = useQuery(ALL_AUTHORS_AND_BOOKS)
 
     if (result.loading) {
         return <div>loading...</div>
@@ -27,7 +27,7 @@ const App = () => {
                 authors={result.data.allAuthors}
             />
 
-            <Books show={page === 'books'} />
+            <Books show={page === 'books'} books={result.data.allBooks} />
 
             <NewBook show={page === 'add'} />
         </div>
--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -1,11 +1,16 @@
 import { gql } from '@apollo/client'
 
-export const ALL_AUTHORS = gql`
+export const ALL_AUTHORS_AND_BOOKS = gql`
     query {
         allAuthors {
             name
             born
             bookCount
+        }
+        allBooks {
+            title
+            author
+            published
         }
     }
 `
