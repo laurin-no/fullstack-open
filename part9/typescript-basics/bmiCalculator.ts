446fe73d5bfb67c989309dc38a665271ca3de878
Laurin Novak <laurin@pellervoinen.com>
2022-12-01 17:06:55 +0200

    implement exercises 9.5

--- part9/typescript-basics/bmiCalculator.ts
+++ part9/typescript-basics/bmiCalculator.ts
@@ -20,7 +20,7 @@ const parseArguments = (args: Array<string>): ArgumentValues => {
     }
 }
 
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
     const bmi = weight / ((height / 100) ** 2)
 
     if (bmi < 19) {
--- part9/typescript-basics/index.ts
+++ part9/typescript-basics/index.ts
@@ -1,9 +1,23 @@
 import express from 'express'
+import { calculateBmi } from './bmiCalculator'
 
 const app = express()
 
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!')
+})
+
+app.get('/bmi', (req, res) => {
+    const height = Number(req.query.height)
+    const weight = Number(req.query.weight)
+
+    if (!height || !weight) {
+        const error = { error: 'malformed parameters' }
+        res.status(400).send(error)
+    }
+
+    const bmi = calculateBmi(height, weight)
+    res.send({ height, weight, bmi })
 })
 
 const PORT = 3003
