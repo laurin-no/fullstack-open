e66e78e3853fa6ed1a78b30d6ccf1481cf6c419e
Laurin Novak <laurin@pellervoinen.com>
2022-12-07 14:52:22 +0200

    implement exercises 9.15

--- part9/halfstack/src/App.tsx
+++ part9/halfstack/src/App.tsx
@@ -1,17 +1,70 @@
 const App = () => {
     const courseName = 'Half Stack application development';
-    const courseParts = [
+
+    interface CoursePartBase {
+        name: string;
+        exerciseCount: number;
+        type: string;
+    }
+
+    interface CoursePartDescriptionBase extends CoursePartBase {
+        description: string;
+    }
+
+    interface CourseNormalPart extends CoursePartDescriptionBase {
+        type: 'normal';
+    }
+
+    interface CourseProjectPart extends CoursePartBase {
+        type: 'groupProject';
+        groupProjectCount: number;
+    }
+
+    interface CourseSubmissionPart extends CoursePartDescriptionBase {
+        type: 'submission';
+        description: string;
+        exerciseSubmissionLink: string;
+    }
+
+    interface CourseSpecialPart extends CoursePartDescriptionBase {
+        type: 'special';
+        requirements: string[];
+    }
+
+    type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart | CourseSpecialPart;
+
+    const courseParts: CoursePart[] = [
         {
             name: 'Fundamentals',
-            exerciseCount: 10
+            exerciseCount: 10,
+            description: 'This is the easy course part',
+            type: 'normal'
+        },
+        {
+            name: 'Advanced',
+            exerciseCount: 7,
+            description: 'This is the hard course part',
+            type: 'normal'
         },
         {
             name: 'Using props to pass data',
-            exerciseCount: 7
+            exerciseCount: 7,
+            groupProjectCount: 3,
+            type: 'groupProject'
         },
         {
             name: 'Deeper type usage',
-            exerciseCount: 14
+            exerciseCount: 14,
+            description: 'Confusing description',
+            exerciseSubmissionLink: 'https://fake-exercise-submit.made-up-url.dev',
+            type: 'submission'
+        },
+        {
+            name: 'Backend development',
+            exerciseCount: 21,
+            description: 'Typing the backend',
+            requirements: ['nodejs', 'jest'],
+            type: 'special'
         }
     ];
 
@@ -23,27 +76,48 @@ const App = () => {
         return <h1>{props.name}</h1>;
     };
 
-    interface Course {
-        name: string,
-        exerciseCount: number
-    }
-
     interface ContentProps {
-        courses: Course[];
+        courses: CoursePart[];
     }
 
     const Content = (props: ContentProps) => {
         return (
             <>
                 {props.courses.map(course =>
-                    <p key={course.name}>{course.name} {course.exerciseCount}</p>
+                    <Part key={course.name} part={course} />
                 )}
             </>
         );
     };
 
+    interface PartProps {
+        part: CoursePart;
+    }
+
+    const assertNever = (value: never): never => {
+        throw  new Error(`Unhandled discriminated union member: ${JSON.stringify(value)}`);
+    };
+    const Part = (props: PartProps) => {
+        switch (props.part.type) {
+            case 'normal':
+                return <p><b>{props.part.name} {props.part.exerciseCount}</b><br /><i>{props.part.description}</i></p>;
+            case 'groupProject':
+                return <p><b>{props.part.name} {props.part.exerciseCount}</b><br />project
+                    exercises {props.part.groupProjectCount}</p>;
+            case 'submission':
+                return <p><b>{props.part.name} {props.part.exerciseCount}</b><br /><i>{props.part.description}</i><br />submit
+                    to {props.part.exerciseSubmissionLink}</p>;
+            case 'special':
+                return <p><b>{props.part.name} {props.part.exerciseCount}</b><br /><i>{props.part.description}</i><br />
+                    required skills: {props.part.requirements.join()}
+                </p>;
+            default:
+                return assertNever(props.part);
+        }
+    };
+
     interface TotalProps {
-        courses: Course[];
+        courses: CoursePart[];
     }
 
     const Total = (props: TotalProps) => {
